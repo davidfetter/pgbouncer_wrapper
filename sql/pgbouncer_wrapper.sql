7cea44c93dd7470179e79a4eaf4784e94ce11b1a
David Fetter <david@farmersbusinessnetwork.com>
2015-03-31 07:27:12 -0700

    Formatting and cleanup

--- sql/pgbouncer_wrapper.sql
+++ sql/pgbouncer_wrapper.sql
@@ -4,28 +4,109 @@
  *
  */
 
---
--- This is a example code genereted automaticaly
--- by pgxn-utils.
-
-SET client_min_messages = warning;
-
--- If your extension will create a type you can
--- do somenthing like this
-CREATE TYPE pgbouncer_wrapper AS ( a text, b text );
-
--- Maybe you want to create some function, so you can use
--- this as an example
-CREATE OR REPLACE FUNCTION pgbouncer_wrapper (text, text)
-RETURNS pgbouncer_wrapper LANGUAGE SQL AS 'SELECT ROW($1, $2)::pgbouncer_wrapper';
-
--- Sometimes it is common to use special operators to
--- work with your new created type, you can create
--- one like the command bellow if it is applicable
--- to your case
-
-CREATE OPERATOR #? (
-	LEFTARG   = text,
-	RIGHTARG  = text,
-	PROCEDURE = pgbouncer_wrapper
+-- customize start
+CREATE SERVER pgbouncer FOREIGN DATA WRAPPER dblink_fdw OPTIONS (
+    host 'localhost',
+    port '6432',
+    dbname 'pgbouncer'
 );
+
+CREATE USER MAPPING FOR PUBLIC SERVER pgbouncer OPTIONS (
+    user 'pgbouncer'
+);
+-- customize stop
+
+CREATE SCHEMA pgbouncer;
+
+CREATE VIEW pgbouncer.clients AS
+    SELECT * FROM dblink('pgbouncer', 'show clients') AS _(
+        type text,
+        "user" text,
+        database text,
+        state text,
+        addr text,
+        port int,
+        local_addr text,
+        local_port int,
+        connect_time timestamp with time zone,
+        request_time timestamp with time zone,
+        ptr text,
+        link text
+    );
+
+CREATE VIEW pgbouncer.config AS
+    SELECT * FROM dblink('pgbouncer', 'show config') AS _(
+        key text,
+        value text,
+        changeable boolean
+    );
+
+CREATE VIEW pgbouncer.databases AS
+    SELECT * FROM dblink('pgbouncer', 'show databases') AS _(
+        name text,
+        host text,
+        port int,
+        database text,
+        force_user text,
+        pool_size int,
+        reserve_pool int
+    );
+
+CREATE VIEW pgbouncer.lists AS
+    SELECT * FROM dblink('pgbouncer', 'show lists') AS _(
+        list text,
+        items int
+    );
+
+CREATE VIEW pgbouncer.pools AS
+    SELECT * FROM dblink('pgbouncer', 'show pools') AS _(
+        database text,
+        "user" text,
+        cl_active int,
+        cl_waiting int,
+        sv_active int,
+        sv_idle int,
+        sv_used int,
+        sv_tested int,
+        sv_login int,
+        maxwait int
+    );
+
+CREATE VIEW pgbouncer.servers AS
+    SELECT * FROM dblink('pgbouncer', 'show servers') AS _(
+        type text,
+        "user" text,
+        database text,
+        state text,
+        addr text,
+        port int,
+        local_addr text,
+        local_port int,
+        connect_time timestamp with time zone,
+        request_time timestamp with time zone,
+        ptr text,
+        link text
+    );
+
+CREATE VIEW pgbouncer.sockets AS
+    SELECT * FROM dblink('pgbouncer', 'show sockets') AS _(
+        type text,
+        "user" text,
+        database text,
+        state text,
+        addr text,
+        port int,
+        local_addr text,
+        local_port int,
+        connect_time timestamp with time zone,
+        request_time timestamp with time zone,
+        ptr text,
+        link text,
+        recv_pos int,
+        pkt_pos int,
+        pkt_remain int,
+        send_pos int,
+        send_remain int,
+        pkt_avail int,
+        send_avail int
+    );
